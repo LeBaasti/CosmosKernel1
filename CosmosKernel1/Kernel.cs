571eb8b4a26f5b10a41d3e19e2d2922c843d86a5
Bastian <BastianSchulz2005@web.de>
2024-10-11 10:33:03 +0200

    commands test

--- CosmosKernel1/Kernel.cs
+++ CosmosKernel1/Kernel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using Sys = Cosmos.System;
 
@@ -10,8 +11,7 @@ namespace CosmosKernel1
 
         public static int memorySize = 16;
         Cosmos.Core.MemoryBlock08[] memory = new Cosmos.Core.MemoryBlock08[memorySize];
-        int uptimeInMilliseconds = Environment.TickCount;
-        TimeSpan uptime = TimeSpan.FromMilliseconds(uptimeInMilliseconds);
+        //Stopwatch timer = Stopwatch.StartNew();
 
         protected override void BeforeRun()
         {
@@ -22,32 +22,36 @@ namespace CosmosKernel1
         {
             Console.Write("Input: ");
             var input = Console.ReadLine();
-            if (input.Equals("tim", StringComparison.OrdinalIgnoreCase))
-            {
-                Console.WriteLine("Tim ist der Schreiber.");
-                Console.WriteLine("Oliver auch");
-            }
-            if (input.Equals("--help", StringComparison.OrdinalIgnoreCase))
-{
-    Console.WriteLine("Usage: [your_program] [options]");
-    Console.WriteLine();
-    Console.WriteLine("Options:");
-    Console.WriteLine("  --help        Display this help message.");
-    Console.WriteLine("  --version     Show the program version.");
-    Console.WriteLine("  --config      Specify the configuration file.");
-    Console.WriteLine("  --verbose     Enable verbose logging.");
-    Console.WriteLine();
-    Console.WriteLine("Example:");
-    Console.WriteLine("  your_program --config=config.json --verbose");
-}
-if (input.Equals("runtime", StringComparison.OrdinalIgnoreCase))
-Console.WriteLine("Systemlaufzeit: {0} Tage, {1} Stunden, {2} Minuten, {3} Sekunden",
-            uptime.Days, uptime.Hours, uptime.Minutes, uptime.Seconds);
 
-            else
+            var args = input.Split(' ');
+            switch(args[0])
             {
-                Console.Write("Text typed: ");
-                Console.WriteLine(input);
+                case "help":
+                    {
+                        Console.WriteLine("Usage: [your_program] [options]");
+                        Console.WriteLine();
+                        Console.WriteLine("Options:");
+                        Console.WriteLine("  --help        Display this help message.");
+                        Console.WriteLine("  --version     Show the program version.");
+                        Console.WriteLine("  --config      Specify the configuration file.");
+                        Console.WriteLine("  --verbose     Enable verbose logging.");
+                        Console.WriteLine();
+                        Console.WriteLine("Example:");
+                        Console.WriteLine("  your_program --config=config.json --verbose");
+                        break;
+                    }
+                case "runtime":
+                    {
+                        //Console.WriteLine(timer.Elapsed.TotalSeconds);
+                        TimeSpan uptime = TimeSpan.FromMilliseconds(Environment.TickCount);
+                        Console.WriteLine("Systemlaufzeit: {0} Tage, {1} Stunden, {2} Minuten, {3} Sekunden",
+                                    uptime.Days, uptime.Hours, uptime.Minutes, uptime.Seconds);
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
             }
         }
     }
