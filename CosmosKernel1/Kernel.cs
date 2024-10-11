3922abcc45cf0ebb5e0c8b412518000f77d110d7
Bastian <BastianSchulz2005@web.de>
2024-10-11 09:49:47 +0200

    Projektdateien hinzufügen.

---
+++ CosmosKernel1/Kernel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace CosmosKernel1
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        public static int memorySize = 16;
+        Cosmos.Core.MemoryBlock08[] memory = new Cosmos.Core.MemoryBlock08[memorySize];
+
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            if (input.Equals("tim", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Tim ist der Schreiber.");
+            }
+            else
+            {
+                Console.Write("Text typed: ");
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
