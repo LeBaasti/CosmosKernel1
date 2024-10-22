0cedc8b9903d131a945ff39c3086a64379551e20
2002seide <62904777+2002seide@users.noreply.github.com>
2024-10-22 08:23:50 +0200

    Add: Benutzerverwaltung, Speicherverwaltung

--- CosmosKernel1/Kernel.cs
+++ CosmosKernel1/Kernel.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Text;
 using Sys = Cosmos.System;
 
+
 namespace CosmosKernel1
 {
     public class Kernel : Sys.Kernel
@@ -13,15 +14,74 @@ namespace CosmosKernel1
         Cosmos.Core.MemoryBlock08[] memory = new Cosmos.Core.MemoryBlock08[memorySize];
 
         public DateTime systemStart;
+        Directory RootDirectory = new Directory("source");
+        Directory CurrentDirectory = new Directory("none");
+
+        ProgramClass CurrentProgram = new Output();
+
+        User LocalMachine = new User("root", eUserLevel.kAdministrator);
+        User CurrentUser = new User("none", eUserLevel.kNone);
+
+        List<User> Users = new List<User>();
 
         protected override void BeforeRun()
         {
             Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
             systemStart = DateTime.Now;
         }
-
         protected override void Run()
         {
+            RootDirectory.AddDirectory(new Directory("etc", LocalMachine));
+
+            while(CurrentUser.userName == "none")
+            {
+                if(Users.Count == 0) 
+                {
+                    Console.Write("Create new user!\nEnter username: ");
+                    string uname = Console.ReadLine();
+                    if(uname.Length >= SystemSettings.MinCharCountUsername) 
+                    {
+                        Console.WriteLine("Enter password: ");
+                        string pass = Console.ReadLine();
+
+                        if(pass.Length >= SystemSettings.MinCharCountPassword)
+                        {
+                            Console.WriteLine("Repeat password: ");
+                            string pass2 = Console.ReadLine();
+
+                            if(pass == pass2)
+                            {
+                                Console.WriteLine("Passwort set!");
+                                User newUser = new User(uname, eUserLevel.kAdministrator);
+                                newUser.SetPassword(pass);
+                                Users.Add(newUser);
+                            }
+                        }
+                    }                    
+                }
+                else
+                {
+                    Console.Write("Log in with username: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("Password: ");
+                    string passw = Console.ReadLine();
+
+                    foreach (User u in Users)
+                    {
+                        if ((u.userName == name) && (u.password == passw))
+                        {
+                            Console.WriteLine("Login successfull!");
+                            CurrentDirectory = RootDirectory;
+                            CurrentUser = u;
+                        }
+                    }
+                }              
+            }
+
+            Console.Write(String.Format("{0} >:", CurrentDirectory.DirectoryName));
+            string command = Console.ReadLine();
+
             Console.Write("Input: ");
             var input = Console.ReadLine();
 
---
+++ CosmosKernel1/SystemSettings.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KernelProject_One
+{
+    public static class SystemSettings
+    {
+        public static readonly int MinCharCountPassword = 4;
+        public static readonly int MinCharCountUsername = 4;
+    }
+}
